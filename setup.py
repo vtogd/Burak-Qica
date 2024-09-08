9b5f4fe8dafeb446f59e60e3acafcdcba5506894
vtogd <vtogd@abv.bg>
2024-09-08 19:19:12 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='burak_qica',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies here
+    ],
+    description='A description of my package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/vtogd/Burak-Qica',
+    author='VTOGD',
+    author_email='vtogd@abv.bg',
+    license='MIT',
+)
